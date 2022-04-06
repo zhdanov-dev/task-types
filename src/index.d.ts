351ca56d9148daeaea10657903a73e5afd1485cc
sergey <zhdanushkaa@yandex.ru>
2022-04-06 13:07:29 +0500

    Задание по типам

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,21 +1,18 @@
-/*
-Типизируйте функцию getPersons, возвращающую массив объектов со следующими свойствами:
-- name - строка
-- age - число
-- gender - 'male' или 'female'
- */
-export function getPersons();
+export function getPersons(): TPerson[];
 
-/*
-Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
-   1. Пользователь:
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   2. Сотрудник
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   - company- строка
- */
-export function personToString(person);
+type TPerson = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+};
+
+type TEmployee = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+    company: string;
+};
+
+type TPersonToString = TPerson | TEmployee;
+
+export function personToString(person: TPersonToString): string;
